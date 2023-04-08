ac8c4cf05417b612e05e10274bcbe357d4ec54e8
sanghyunbak <sanghyunbak@gmail.com>
2023-04-09 08:37:35 +0900

    [FEATURE] add onSubmit function

--- src/components/chapter10_todolist/TodoInsert.js
+++ src/components/chapter10_todolist/TodoInsert.js
@@ -2,7 +2,7 @@ import {MdAdd} from 'react-icons/md' // md: material design
 import React, {useCallback, useState} from 'react';
 import './TodoInsert.scss'
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
   const [value, setValue] = useState('');
 
   const onChange = useCallback(
@@ -10,6 +10,18 @@ const TodoInsert = () => {
         setValue(e.target.value)
       },
       [],
+  );
+
+  const onSubmit = useCallback(
+      e => {
+        onInsert(value);
+        setValue(''); // initialize value
+
+        // submit event make browser's refresh
+        // so call this function to prevent this
+        e.preventDefault();
+      },
+      [onInsert, value],
   );
 
   return (
